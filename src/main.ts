3c462110edd5c9a502dfd7eaee74e604734740ec
Mobolaji Richard <sinclairrichard79@gmail.com>
2025-02-19 10:26:58 +0100

    cors issue resolved

--- src/main.ts
+++ src/main.ts
@@ -6,6 +6,11 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.useGlobalPipes(new ValidationPipe())
   app.setGlobalPrefix('api/v1');
+  app.enableCors({
+    origin: '*',
+    methods: ['GET', 'POST', 'PUT', 'DELETE', 'PATCH', 'OPTIONS'],
+    allowedHeaders: ['Content-Type', 'Authorization'],
+  });
   await app.listen(process.env.PORT ?? 5050);
 }
 bootstrap();
